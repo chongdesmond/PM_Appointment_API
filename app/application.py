26e6c03a3d291fed22ea9198aab526b0bd3d182d
chongdesmond <78634252+chongdesmond@users.noreply.github.com>
2021-04-17 18:32:56 +0800

    Update application.py

--- app/application.py
+++ app/application.py
@@ -6,7 +6,6 @@ import calendar
 
 app = Flask(__name__)
 
-print("A")
 DB_URI = mongodb_uri.format(mongodb_password, mongodb_name)
 app.config["MONGODB_HOST"] = DB_URI
 
@@ -14,57 +13,67 @@ db = MongoEngine()
 db.init_app(app)
 
 ##### Patient Medical Data #####
+
 class PatientMedicalData(db.Document):
-    PatientId = db.StringField()
+    PatientMedId = db.SequenceField()
+    PatientId = db.IntField()
     Symptom = db.StringField()
     PreferredDate = db.DateTimeField()
     PreferredTime = db.StringField()
 
     def to_json(self):
         return {
+            "PatientMedId": self.PatientMedId,
             "PatientId": self.PatientId,
             "Symptom": self.Symptom,
             "PreferredDate": self.PreferredDate,
             "PreferredTime": self.PreferredTime
         }
 
+
 @app.route('/api/patient_medical_data/populate', methods=['POST'])
 def patient_populate():
-    obj1 = PatientMedicalData(PatientId="1", Symptom="fever,running nose", PreferredDate=datetime.datetime(2021,3,27, 0 , 0), PreferredTime="10:00am")
-    obj2 = PatientMedicalData(PatientId="2", Symptom="headache,vommit", PreferredDate=datetime.datetime(2021,4,5, 0 , 0), PreferredTime="03:00pm")
-    obj3 = PatientMedicalData(PatientId="3", Symptom="amnesia,headache,", PreferredDate=datetime.datetime(2021,3,30, 0 , 0), PreferredTime="05:30pm")
-    obj4 = PatientMedicalData(PatientId="4", Symptom="flu,headache,muscle pain", PreferredDate=datetime.datetime(2021,4,20, 0 , 0), PreferredTime="12:00pm")
-    obj5 = PatientMedicalData(PatientId="5", Symptom="vommit,cough", PreferredDate=datetime.datetime(2021,4,11, 0 , 0), PreferredTime="09:30am")
-    
+    obj1 = PatientMedicalData(PatientId=1, Symptom="fever,running nose", PreferredDate=datetime.datetime.strptime("2021-03-27T00:00:00+08:00", datetime_format), PreferredTime="10:00am")
+    obj2 = PatientMedicalData(PatientId=2, Symptom="headache,vommit", PreferredDate=datetime.datetime.strptime("2021-04-05T00:00:00+08:00", datetime_format), PreferredTime="03:00pm")
+    obj3 = PatientMedicalData(PatientId=3, Symptom="amnesia,headache,", PreferredDate=datetime.datetime.strptime("2021-03-30T00:00:00+08:00", datetime_format), PreferredTime="05:30pm")
+    obj4 = PatientMedicalData(PatientId=4, Symptom="flu,headache,muscle pain", PreferredDate=datetime.datetime.strptime("2021-04-20T00:00:00+08:00", datetime_format), PreferredTime="12:00pm")
+    obj5 = PatientMedicalData(PatientId=5, Symptom="vommit,cough", PreferredDate=datetime.datetime.strptime("2021-05-30T00:00:00+08:00", datetime_format), PreferredTime="09:30am")
+
     obj1.save()
     obj2.save()
     obj3.save()
     obj4.save()
     obj5.save()
 
-    return make_response("Populated successfully",201)
+    return make_response("Populated successfully", 201)
 
-@app.route('/api/patient_medical_datas', methods=['GET','POST'])
+
+@app.route('/api/patient_medical_datas', methods=['GET', 'POST'])
 def api_patient_med_data():
     if request.method == "GET":
         datas = []
         for data in PatientMedicalData.objects:
-            datas.append(data)
+            data.PreferredDate = data.PreferredDate.isoformat()
+            datas.append(data.to_json())
 
         return make_response(jsonify(datas), 200)
 
     elif request.method == "POST":
         content = request.json
-        
+
         data = PatientMedicalData(PatientId=content['PatientId'], Symptom=content['Symptom'], PreferredDate=datetime.datetime.strptime(content['PreferredDate'], datetime_format), PreferredTime=content['PreferredTime'])
         data.save()
 
-        return make_response("Inserted successfully",201)
+        #data.PreferredDate = data.PreferredDate.strftime(datetime_format)
 
-@app.route('/api/patient_medical_datas/<patient_id>', methods=['GET','PUT','DELETE'])
-def api_each_patient_med_data(patient_id):
+        return make_response(jsonify(data.PatientMedId), 201)
+
+
+@app.route('/api/patient_medical_datas/<patient_med_id>', methods=['GET', 'PUT', 'DELETE'])
+def api_each_patient_med_data(patient_med_id):
     if request.method == "GET":
-        obj = PatientMedicalData.objects(PatientId=patient_id).first()
+        obj = PatientMedicalData.objects(PatientMedId=patient_med_id).first()
+        obj.PreferredDate = obj.PreferredDate.isoformat()
         if obj:
             return make_response(jsonify(obj.to_json()), 200)
         else:
@@ -72,20 +81,33 @@ def api_each_patient_med_data(patient_id):
 
     elif request.method == "PUT":
         content = request.json
-        obj = PatientMedicalData.objects(PatientId=patient_id).first()
-        obj.update(Symptom=content['Symptom'], PreferredDate=datetime.datetime.strptime(content['PreferredDate'], datetime_format), PreferredTime=content['PreferredTime'])
+        obj = PatientMedicalData.objects(PatientMedId=patient_med_id).first()
+        obj.update(PatientId=content['PatientId'], Symptom=content['Symptom'], PreferredDate=datetime.datetime.strptime(content['PreferredDate'], datetime_format), PreferredTime=content['PreferredTime'])
 
         return make_response("Updated successfully", 204)
 
     elif request.method == "DELETE":
-        obj = PatientMedicalData.objects(PatientId=patient_id).first()
+        obj = PatientMedicalData.objects(PatientMedId=patient_med_id).first()
         obj.delete()
 
         return make_response("Deleted successfully", 204)
 
+@app.route('/api/medical_datas_by_patient/<patient_id>', methods=['GET'])
+def api_medical_datas_by_patient(patient_id):
+    if request.method == "GET":
+        datas = []
+        for data in PatientMedicalData.objects(PatientId=patient_id):
+            data.PreferredDate = data.PreferredDate.isoformat()
+            datas.append(data.to_json())
+
+        return make_response(jsonify(datas), 200)
+
+
 ##### DoctorSpeciality #####
+
 class DoctorSpeciality(db.Document):
-    DoctorId = db.StringField()
+    DoctorSpecId = db.SequenceField()
+    DoctorId = db.IntField()
     HandledSymptoms = db.StringField()
     AvailableDay = db.StringField()
     AvailableTime = db.StringField()
@@ -94,6 +116,7 @@ class DoctorSpeciality(db.Document):
 
     def to_json(self):
         return {
+            "DoctorSpecId": self.DoctorSpecId,
             "DoctorId": self.DoctorId,
             "HandledSymptoms": self.HandledSymptoms,
             "AvailableDay": self.AvailableDay,
@@ -102,43 +125,46 @@ class DoctorSpeciality(db.Document):
             "ChargeRate": self.ChargeRate
         }
 
+
 @app.route('/api/doctor_speciality/populate', methods=['POST'])
 def doctor_populate():
-    obj1 = DoctorSpeciality(DoctorId="1", HandledSymptoms="fever,vommit", AvailableDay="Saturday,Sunday", AvailableTime="07:00pm",Rating=3,ChargeRate=40.5)
-    obj2 = DoctorSpeciality(DoctorId="2", HandledSymptoms="headache,amnesia", AvailableDay="Sunday", AvailableTime="10:00am",Rating=1,ChargeRate=50)
-    obj3 = DoctorSpeciality(DoctorId="3", HandledSymptoms="muscle pain,headache,", AvailableDay="Monday,Wednesday", AvailableTime="03:00pm",Rating=5,ChargeRate=60)
-    obj4 = DoctorSpeciality(DoctorId="4", HandledSymptoms="flu,running nose,headache", AvailableDay="Saturday", AvailableTime="04:00pm",Rating=4,ChargeRate=25)
-    obj5 = DoctorSpeciality(DoctorId="5", HandledSymptoms="fever,cough", AvailableDay="Friday,Saturday", AvailableTime="06:30pm",Rating=3,ChargeRate=30)
-    
+    obj1 = DoctorSpeciality(DoctorId=1,HandledSymptoms="fever,vommit", AvailableDay="Saturday,Sunday", AvailableTime="07:00pm", Rating=3, ChargeRate=40.5)
+    obj2 = DoctorSpeciality(DoctorId=2,HandledSymptoms="headache,amnesia", AvailableDay="Sunday", AvailableTime="10:00am", Rating=1, ChargeRate=50)
+    obj3 = DoctorSpeciality(DoctorId=3,HandledSymptoms="muscle pain,headache,", AvailableDay="Monday,Wednesday", AvailableTime="03:00pm", Rating=5, ChargeRate=60)
+    obj4 = DoctorSpeciality(DoctorId=4,HandledSymptoms="flu,running nose,headache", AvailableDay="Saturday", AvailableTime="04:00pm", Rating=4, ChargeRate=25)
+    obj5 = DoctorSpeciality(DoctorId=5,HandledSymptoms="fever,cough", AvailableDay="Friday,Saturday", AvailableTime="06:30pm", Rating=3, ChargeRate=30)
+
     obj1.save()
     obj2.save()
     obj3.save()
     obj4.save()
     obj5.save()
 
-    return make_response("Populated successfully",201)
+    return make_response("Populated successfully", 201)
 
-@app.route('/api/doctor_speciality_datas', methods=['GET','POST'])
+
+@app.route('/api/doctor_speciality_datas', methods=['GET', 'POST'])
 def api_doctor_spec_data():
     if request.method == "GET":
         datas = []
         for data in DoctorSpeciality.objects:
             datas.append(data)
-        print (type(datas))
+
         return make_response(jsonify(datas), 200)
 
     elif request.method == "POST":
         content = request.json
-        
+
         data = DoctorSpeciality(DoctorId=content['DoctorId'], HandledSymptoms=content['HandledSymptoms'], AvailableDay=content['AvailableDay'], AvailableTime=content['AvailableTime'], Rating=content['Rating'], ChargeRate=content['ChargeRate'])
         data.save()
 
-        return make_response("Inserted successfully",201)
+        return make_response("Inserted successfully", 201)
 
-@app.route('/api/doctor_speciality_datas/<doctor_id>', methods=['GET','PUT','DELETE'])
-def api_each_doctor_spec_data(doctor_id):
+
+@app.route('/api/doctor_speciality_datas/<doctor_spec_id>', methods=['GET', 'PUT', 'DELETE'])
+def api_each_doctor_spec_data(doctor_spec_id):
     if request.method == "GET":
-        obj = DoctorSpeciality.objects(DoctorId=doctor_id).first()
+        obj = DoctorSpeciality.objects(DoctorSpecId=doctor_spec_id).first()
         if obj:
             return make_response(jsonify(obj.to_json()), 200)
         else:
@@ -146,31 +172,149 @@ def api_each_doctor_spec_data(doctor_id):
 
     elif request.method == "PUT":
         content = request.json
-        obj = DoctorSpeciality.objects(DoctorId=doctor_id).first()
-        obj.update(HandledSymptoms=content['HandledSymptoms'], AvailableDay=content['AvailableDay'], AvailableTime=content['AvailableTime'], Rating=content['Rating'], ChargeRate=content['ChargeRate'])
+        obj = DoctorSpeciality.objects(DoctorSpecId=doctor_spec_id).first()
+        obj.update(DoctorId=content['DoctorId'], HandledSymptoms=content['HandledSymptoms'], AvailableDay=content['AvailableDay'], AvailableTime=content['AvailableTime'], Rating=content['Rating'], ChargeRate=content['ChargeRate'])
 
         return make_response("Updated successfully", 204)
 
     elif request.method == "DELETE":
-        obj = DoctorSpeciality.objects(DoctorId=doctor_id).first()
+        obj = DoctorSpeciality.objects(DoctorSpecId=doctor_spec_id).first()
         obj.delete()
-        
+
         return make_response("Deleted successfully", 204)
+
+@app.route('/api/doctor_spec_by_doctor/<doctor_id>', methods=['GET'])
+def api_doctor_spec_by_doctor(doctor_id):
+    if request.method == "GET":
+        datas = []
+        for data in DoctorSpeciality.objects(DoctorId=doctor_id):
+            datas.append(data)
+
+        return make_response(jsonify(datas), 200)
+
+
+##### Appointment API ####
+
+class MedicalAppointment(db.Document):
+    AppointmentId = db.SequenceField()
+    AppointmentNumber = db.StringField()
+    PatientMedId = db.IntField()
+    DoctorSpecId = db.IntField()
+    Status = db.StringField()
+    AppointmentDate = db.DateTimeField()
+
+    def to_json(self):
+        return {
+            "AppointmentId": self.AppointmentId,
+            "AppointmentNumber": self.AppointmentNumber,
+            "PatientMedId": self.PatientMedId,
+            "DoctorSpecId": self.DoctorSpecId,
+            "Status": self.Status,
+            "AppointmentDate": self.AppointmentDate
+        }
+
+
+@app.route('/api/medicalappointment/populate', methods=['POST'])
+def medical_app_populate():
+    obj1 = MedicalAppointment(AppointmentNumber="10", PatientMedId="1", DoctorSpecId="1", Status="A", AppointmentDate=datetime.datetime(2021, 3, 27, 9, 0))
+    obj2 = MedicalAppointment(AppointmentNumber="20", PatientMedId="2", DoctorSpecId="2", Status="A", AppointmentDate=datetime.datetime(2021, 4, 2, 17, 0))
+    obj3 = MedicalAppointment(AppointmentNumber="30", PatientMedId="3", DoctorSpecId="3", Status="A", AppointmentDate=datetime.datetime(2021, 5, 14, 15, 0))
+    obj4 = MedicalAppointment(AppointmentNumber="40", PatientMedId="4", DoctorSpecId="4", Status="A", AppointmentDate=datetime.datetime(2021, 6, 8, 12, 0))
+    obj5 = MedicalAppointment(AppointmentNumber="50", PatientMedId="5", DoctorSpecId="5", Status="A", AppointmentDate=datetime.datetime(2021, 4, 5, 15, 0))
+
+    obj1.save()
+    obj2.save()
+    obj3.save()
+    obj4.save()
+    obj5.save()
+
+    return make_response("PatientMedicalData populated successfully", 201)
+
+
+@app.route('/api/medicalappointments', methods=['GET', 'POST'])
+def api_medicalapp_data():
+    if request.method == "GET":
+        datas = []
+        for data in MedicalAppointment.objects:
+            data.AppointmentDate = data.AppointmentDate.isoformat()
+            datas.append(data.to_json())
+
+        return make_response(jsonify(datas), 200)
+
+    elif request.method == "POST":
+        content = request.json
+
+        data = MedicalAppointment(AppointmentNumber=content['AppointmentNumber'], PatientMedId=content['PatientMedId'], DoctorSpecId=content['DoctorSpecId'], Status=content['Status'], AppointmentDate=datetime.datetime.strptime(content['AppointmentDate'], datetime_format))
+        data.save()
+
+        data.AppointmentDate = data.AppointmentDate.isoformat()
+
+        return make_response(jsonify(data), 201)
+
+
+@app.route('/api/medicalappointments/<appointment_id>', methods=['GET', 'PUT', 'DELETE'])
+def api_each_medicalapp_data(appointment_id):
+    if request.method == "GET":
+        obj = MedicalAppointment.objects(AppointmentId=appointment_id).first()
+        obj.AppointmentDate = obj.AppointmentDate.isoformat()
+        if obj:
+            return make_response(jsonify(obj.to_json()), 200)
+        else:
+            return make_response("Data not found", 404)
+
+    elif request.method == "PUT":
+        content = request.json
+        obj = MedicalAppointment.objects(AppointmentId=appointment_id).first()
+        obj.update(AppointmentNumber=content['AppointmentNumber'], PatientMedId=content['PatientMedId'], DoctorSpecId=content['DoctorSpecId'], Status=content['Status'], AppointmentDate=datetime.datetime.strptime(content['AppointmentDate'], datetime_format))
+
+        return make_response("Updated successfully", 204)
+
+    elif request.method == "DELETE":
+        obj = MedicalAppointment.objects(AppointmentId=appointment_id).first()
+        obj.delete()
+
+        return make_response("Deleted successfully", 204)
+
+
+@app.route('/api/update_med_app_number/<appointment_id>', methods=['PUT'])
+def api_update_med_app_number(appointment_id):
+    if request.method == "PUT":
+        content = request.json
+        obj = MedicalAppointment.objects(AppointmentId=appointment_id).first()
+        obj.update(AppointmentNumber=content['AppointmentNumber'])
+
+        return make_response("Updated successfully", 204)
+
+
+@app.route('/api/update_med_app_status/<appointment_id>', methods=['PUT'])
+def api_update_med_app_status(appointment_id):
+    if request.method == "PUT":
+        content = request.json
+        obj = MedicalAppointment.objects(AppointmentId=appointment_id).first()
+        obj.update(Status=content['Status'])
+
+        return make_response("Updated successfully", 204)
+
 
 ##### DoctorRecommendation #####
 @app.route('/api/getDoctorRanking', methods=['POST']) 
 def getDoctorRanking():
+
     input_map = request.json
-    patient_id= input_map.get("PatientId")
     #symptoms = []
+    patient_med_id= input_map.get("PatientMedId")
     symptoms= input_map.get("Symptom")
     date= input_map.get("PreferredDate")
     time= input_map.get("PreferredTime")
     #datetime_object = datetime.strptime(available_time, '%m/%d/%Y')
-    print ("A")
+    
+    if symptoms is None:
+        return make_response("Data not found", 404)
+
     #calculate the weekday for particular date
     def findDay(date): 
-        week1 = datetime.datetime.strptime(date, '%m/%d/%Y').weekday() 
+        # week1 = datetime.datetime.strptime(date, '%m/%d/%Y').weekday() 
+        week1 = datetime.datetime.strptime(date, datetime_format).weekday() 
         return (calendar.day_name[week1])
 
     Actual_Date= findDay(date)
@@ -179,7 +323,7 @@ def getDoctorRanking():
     dummy_doctor_profiles = []
     for dummy_doctor_profiles1 in DoctorSpeciality.objects:
         dummy_doctor_profiles.append(dummy_doctor_profiles1)
-   
+
     # loop doctor profiles and compare with patient symptoms to find the best doctor.
     # Can assign score for each doctor and rank according using "sorted" function 
     
@@ -197,6 +341,8 @@ def getDoctorRanking():
     #Get all doctor profile from DB and doing recommendation 
     for profile in dummy_doctor_profiles:
         
+        Doctor_spec_id = str(profile["DoctorSpecId"])
+        Doctor_id = str(profile["DoctorId"])
         Doctor_date = profile["AvailableDay"]
         Doctor_time = profile["AvailableTime"]
         str1 = profile["HandledSymptoms"]
@@ -205,7 +351,9 @@ def getDoctorRanking():
 
         if matched_symptoms:
             #symptoms matched and save doctor available date and time for reference if patient have different preference
-            Doctors_date.append((str1 + "     " + Doctor_date + "   " +Doctor_time))
+            # Doctors_date.append("{DoctorId:" + Doctor_id + ",HandledSymptoms:" + str1 + ",AvailableDay:" + Doctor_date + ",AvailableTime:" + Doctor_time + "}")
+            Doctors_date.append(DoctorSpeciality.objects(DoctorSpecId=Doctor_spec_id).first())
+
             Doctor_time = datetime.datetime.strptime(Doctor_time, '%I:%M%p').time()
             if Actual_Date in Doctor_date:
                 if Actual_time == Doctor_time:
@@ -224,14 +372,20 @@ def getDoctorRanking():
 
     if not (ranked_doctor_profiles or Doctors_date):
         #no matching on symptoms
-        return jsonify('no specialise doctor')
+        # return jsonify('no specialise doctor')
+        return make_response(jsonify("Data not found", ""), 404)
+
     elif not ranked_doctor_profiles:
+        #print("no suitable time slot!")
         #symptoms matched but different timing
-        return jsonify("no suitable time slot!", "Others available doctor time:", Doctors_date)
+        # return jsonify("no suitable time slot!", "Others available doctor time:", Doctors_date)
+        return make_response(jsonify(Doctors_date, "No suitable time slot!"), 200)
+
     else:
         #print (ranked_doctor_profiles['score'])
         filtered_profile  = [ obj for obj in ranked_doctor_profiles if obj['Rating'] > 0.0]
-        return jsonify(filtered_profile)
+        # return jsonify(filtered_profile) 
+        return make_response(jsonify(filtered_profile, ""), 200)
 
 ###################
 
