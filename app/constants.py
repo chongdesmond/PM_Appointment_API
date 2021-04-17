f686b5b50b318f6d570d2f29449d6610ea822902
chongdesmond <78634252+chongdesmond@users.noreply.github.com>
2021-04-17 18:33:46 +0800

    Update constants.py

--- app/constants.py
+++ app/constants.py
@@ -1,4 +1,5 @@
 mongodb_uri="mongodb+srv://msazili:{}@cluster0.on4rc.mongodb.net/{}?retryWrites=true&w=majority&authSource=admin"
 mongodb_name="APPOINTMENT"
 mongodb_password="msazili"
-datetime_format="%Y-%m-%d %H:%M:%S.%f"
+datetime_format="%Y-%m-%dT%H:%M:%S%z"
+# datetime_format="%Y-%m-%d %H:%M:%S.%f"
