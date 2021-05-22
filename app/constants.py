7ae1917980d90785810c67479feb2d6cc963a79d
chongdesmond <78634252+chongdesmond@users.noreply.github.com>
2021-05-22 23:29:13 +0800

    Update constants.py

--- app/constants.py
+++ app/constants.py
@@ -1,5 +1,5 @@
 mongodb_uri="mongodb+srv://msazili:{}@cluster0.on4rc.mongodb.net/{}?retryWrites=true&w=majority&authSource=admin"
 mongodb_name="APPOINTMENT"
-mongodb_password="msazili"
+mongodb_password=${{ secrets.ATLAS_PASSWORD}}
 datetime_format="%Y-%m-%dT%H:%M:%S%z"
 # datetime_format="%Y-%m-%d %H:%M:%S.%f"
